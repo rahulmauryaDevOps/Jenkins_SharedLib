2f6aff81001024d971cac20204a72074667a2283
Rahul Maurya <rahul563maurya@gmail.com>
2026-09-13 15:38:23 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,6 @@
 def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+        dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY}", odcInstallation: 'OWASP'
+    }
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
